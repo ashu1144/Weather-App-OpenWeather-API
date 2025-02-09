f3a9bd16002718ee3ee5f7f933567e238f593773
shaik mohammad ashan <124565827+ashu1144@users.noreply.github.com>
2025-02-09 19:40:50 +0530

    Add files via upload

---
+++ whether/index.js
@@ -0,0 +1,57 @@
+let city = document.querySelector("input");
+let search =  document.querySelector("button")
+var data;
+const apiKey = "2e305a3ab9175462a40d6f24584b7c9d";
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=`;
+const weatherIcon = document.querySelector(".weather_icon")
+
+async function checkWeather(params) {
+    const response = await fetch(apiUrl +`${city.value}&units=metric&appid=${apiKey}`)
+    if(response.status == 404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+    }
+    else{
+        document.querySelector(".error").style.display="none";
+        document.querySelector(".weather").style.display="block";
+
+    }
+    data  =  await response.json();
+    
+}
+
+const setWeather = ()=>{
+    if(data.weather[0].main=="Clouds"){
+        weatherIcon.src="images/clouds.png"
+    }
+    else if(data.weather[0].main=="Clear"){
+        weatherIcon.src="images/clear.png"
+    }
+    else if(data.weather[0].main=="Rain"){
+        weatherIcon.src="images/rain.png"
+    }
+    else if(data.weather[0].main=="Drizzle"){
+        weatherIcon.src="images/drizzle.png"
+    }
+    else if(data.weather[0].main=="Mist"){
+        weatherIcon.src="images/mist.png"
+    }
+
+}
+
+search.addEventListener("click", function() {
+    checkWeather()
+    setTimeout(() => {
+        setWeather()
+        document.querySelector(".city").innerText = data.name;
+        document.querySelector(".temp").innerText=`${Math.floor(data.main.temp)}°C`
+        document.querySelector(".humidity").innerText=`${data.main.humidity}`       
+        document.querySelector(".wind").innerText=`${data.wind.speed}km/h`.toUpperCase();
+        document.querySelector("#deg").innerText=`Deg:${data.wind.deg}`
+    }, 1000);
+
+
+  });
+
+
+
